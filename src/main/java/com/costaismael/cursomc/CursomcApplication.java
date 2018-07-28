9b0d7c25dc9560afcef6598e9aefd887b44b8df9
Ismael Costa <ismael.antiqueira@gmail.com>
2018-07-28 12:56:57 -0300

    pedido pagamento estadopagamento

--- src/main/java/com/costaismael/cursomc/CursomcApplication.java
+++ src/main/java/com/costaismael/cursomc/CursomcApplication.java
@@ -1,5 +1,6 @@
 package com.costaismael.cursomc;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,13 +13,20 @@ import com.costaismael.cursomc.domain.Cidade;
 import com.costaismael.cursomc.domain.Cliente;
 import com.costaismael.cursomc.domain.Endereco;
 import com.costaismael.cursomc.domain.Estado;
+import com.costaismael.cursomc.domain.Pagamento;
+import com.costaismael.cursomc.domain.PagamentoComBoleto;
+import com.costaismael.cursomc.domain.PagamentoComCartao;
+import com.costaismael.cursomc.domain.Pedido;
 import com.costaismael.cursomc.domain.Produto;
+import com.costaismael.cursomc.domain.enums.EstadoPagamento;
 import com.costaismael.cursomc.domain.enums.TipoCliente;
 import com.costaismael.cursomc.repositories.CategoriaRepository;
 import com.costaismael.cursomc.repositories.CidadeRepository;
 import com.costaismael.cursomc.repositories.ClienteRepository;
 import com.costaismael.cursomc.repositories.EnderecoRepository;
 import com.costaismael.cursomc.repositories.EstadoRepository;
+import com.costaismael.cursomc.repositories.PagamentoRepository;
+import com.costaismael.cursomc.repositories.PedidoRepository;
 import com.costaismael.cursomc.repositories.ProdutoRepository;
 
 @SpringBootApplication
@@ -36,6 +44,10 @@ public class CursomcApplication implements CommandLineRunner{
 	private ClienteRepository clienteRepository;
 	@Autowired
 	private EnderecoRepository enderecoRepository;	
+	@Autowired
+	private PedidoRepository pedidoRepository;
+	@Autowired
+	private PagamentoRepository pagamentoRepository;	
 	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -87,5 +99,20 @@ public class CursomcApplication implements CommandLineRunner{
 		clienteRepository.save(Arrays.asList(cli1));
 		enderecoRepository.save(Arrays.asList(e1,e2));
 		
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm");
+		
+		Pedido ped1 = new Pedido(null, sdf.parse("30/09/2017 10:32"), cli1, e1);
+		Pedido ped2 = new Pedido(null, sdf.parse("10/10/2017 09:32"), cli1, e2);
+		
+		Pagamento pagto1 = new PagamentoComCartao(null, EstadoPagamento.QUITADO, ped1, 6);
+		ped1.setPagamento(pagto1);
+	
+		Pagamento pagto2 = new PagamentoComBoleto(null, EstadoPagamento.PENDENTE, ped2, sdf.parse("20/10/2017 00:00"), null);
+		ped2.setPagamento(pagto2);
+			
+		cli1.getPedidos().addAll(Arrays.asList(ped1,ped2));
+		
+		pedidoRepository.save(Arrays.asList(ped1,ped2));
+		pagamentoRepository.save(Arrays.asList(pagto1,pagto2));		
 	}
 }
